8c01f90761cb7e9d2ffce17e533d142de4c2cfc5
frautn <epalazzo@fra.utn.edu.ar>
2026-08-14 19:59:44 -0300

    Changed to PostgreSQL

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()  # Automatically finds .env file
 django_secret_key = os.getenv('DJANGO_SECRET_KEY')
+postgres_db = os.getenv('POSTGRES_DB')
+postgres_user = os.getenv('POSTGRES_USER')
+postgres_password = os.getenv('POSTGRES_PASSWORD')
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,13 +83,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  #
+        'NAME': postgres_db,
+        'USER': postgres_user,
+        'PASSWORD': postgres_password,
+        'HOST': 'localhost',  # Use the Docker container name (e.g., 'db') if Django is also inside Docker
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
